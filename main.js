941baf1801614b8691fab144abb26e6f8e8c4f83
by-yz <153606601+by-yz@users.noreply.github.com>
2024-02-26 17:38:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,15 @@ $(document).ready(function () {
     $('#send_quizz1').click(function () {
         var question = {
             'valeur1': $('#question1').val(),
-            'valeur2': $("#name1").val(),
-
+            'valeur2': $("#question2").val(),
+            'valeur3': $('#question3').val(),
+            'valeur4': $("#question4").val(),
+            'valeur5': $('#question5').val(),
+            'valeur6': $("#question6").val(),
+            'valeur7': $('#question7').val(),
+            'valeur8': $("#question8").val(),
+            'valeur9': $("#question9").val(),
+            'valeur10': $('#name').val(),
 
         };
         $.ajax({
@@ -13,7 +20,7 @@ $(document).ready(function () {
             data: question,
             success: function (data) {
                 if (data.succes) {
-                    //window.location.href = "index.html";
+                    window.location.href = "index.html";
                     console.log("transfert terminé. Valeurs reçues : " + (data));
                 }
                 else {
@@ -26,22 +33,38 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function () {
+    $('#send_quizz1').click(function () {
+        var question = {
+            'valeur1': $('#question1').val(),
+            'valeur2': $("#question2").val(),
+            'valeur3': $('#question3').val(),
+            'valeur4': $("#question4").val(),
+            'valeur5': $('#question5').val(),
+            'valeur6': $("#question6").val(),
+            'valeur7': $('#question7').val(),
+            'valeur8': $("#question8").val(),
+            'valeur9': $("#question9").val(),
+            'valeur10': $('#name').val(),
 
-$.ajax({
-    url: 'http://127.0.0.1/db_connect.php',
-    type: 'GET',
-    dataType: 'json',
-    success: function (data) {
-        // Manipulez les données ici (exemple d'affichage dans la console)
-        console.log(data);
+        };
+        $.ajax({
+            url: 'http://127.0.0.1/submit.php',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                // Manipulez les données ici (exemple d'affichage dans la console)
+                console.log(data);
 
-        // Affichez les données sur la page (exemple d'ajout dans une div)
-        var resultDiv = $('#result');
-        data.forEach(function (item) {
-            resultDiv.append('<p>' + item.column_name + '</p>');
-        });
-    },
-    error: function (error) {
-        console.log('Erreur Ajax: ', error);
-    }
+                // Affichez les données sur la page (exemple d'ajout dans une div)
+                var resultDiv = $('#result');
+                data.forEach(function (data) {
+                    resultDiv.append('<p>' + data.question + '</p>');
+                });
+            },
+            error: function (error) {
+                console.log('Erreur Ajax: ', error);
+            }
+        })
+    });
 });
