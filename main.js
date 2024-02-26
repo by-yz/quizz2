36344d035625e749fa29424d9b5952187214008d
by-yz <153606601+by-yz@users.noreply.github.com>
2024-02-26 17:55:53 +0100

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    $('#send_quizz1').click(function () {
+    $('#validate-btn').click(function () {
         var question = {
             'valeur1': $('#question1').val(),
             'valeur2': $("#question2").val(),
