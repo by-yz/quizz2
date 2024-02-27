857ea357ac3f5a4c4ebb2c9c45f066181855cf63
by-yz <153606601+by-yz@users.noreply.github.com>
2024-02-27 11:27:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
     $('#send_quizz1').click(function () {
-        var question = {
-            'valeur1': $('#question1').val(),
-            'valeur2': $("#question2").val(),
-            'valeur3': $('#question3').val(),
-            'valeur4': $("#question4").val(),
-            'valeur5': $('#question5').val(),
-            'valeur6': $("#question6").val(),
-            'valeur7': $('#question7').val(),
-            'valeur8': $("#question8").val(),
-            'valeur9': $('#name').val(),
+        //var questin = {
+          //  'valeur1': $('#question1').val(),
+            //'valeur2': $("#question2").val(),
+            //'valeur3': $('#question3').val(),
+            //'valeur4': $("#question4").val(),
+            //'valeur5': $('#question5').val(),
+            //'valeur6': $("#question6").val(),
+            //'valeur7': $('#question7').val(),
+            //'valeur8': $("#question8").val(),
+            //'valeur9': $("#question9").val(),
+            //'valeur': $('#name').val(),
 
         };
         $.ajax({
@@ -57,7 +58,7 @@ $(document).ready(function () {
                 // Affichez les données sur la page (exemple d'ajout dans une div)
                 var resultDiv = $('#result');
                 data.forEach(function (data) {
-                    resultDiv.append('<p>' + data.question + '</p>');
+                    //resultDiv.append('<p>' + data.question + '</p>');
                 });
             },
             error: function (error) {
